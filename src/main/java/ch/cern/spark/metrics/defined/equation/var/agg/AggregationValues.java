7a981c0f279b25839bc3de6fbf6923af65606c42
daniel.lanza@cern.ch <dlanza@194.12.157.169>
2018-04-25 12:14:22 +0200

    AggregationValues: lastAggregatedMetrics might be null from serializati

--- src/main/java/ch/cern/spark/metrics/defined/equation/var/agg/AggregationValues.java
+++ src/main/java/ch/cern/spark/metrics/defined/equation/var/agg/AggregationValues.java
@@ -46,6 +46,8 @@ public class AggregationValues extends StatusValue {
     }
     
 	public void add(int hash, Value value, Instant timestamp, Metric metric) {
+	    if(lastAggregatedMetrics == null)
+	        lastAggregatedMetrics = new LinkedHashMap<>();
 		lastAggregatedMetrics.put(hash, metric);
 		
 		add(hash, value, timestamp);
