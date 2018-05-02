afd9f5ff69bf0ecf6cfa036de3b6c4a8d7405342
dlanza <dlanza@newnewmacbookprodlanza.dyndns.cern.ch>
2018-05-02 15:22:35 +0200

    Status: log info message for status operations

--- src/main/java/ch/cern/spark/status/Status.java
+++ src/main/java/ch/cern/spark/status/Status.java
@@ -3,6 +3,7 @@ package ch.cern.spark.status;
 import java.io.IOException;
 import java.util.Optional;
 
+import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -26,6 +27,8 @@ import scala.Tuple2;
 public class Status {
 	
 	public static final String STATUSES_EXPIRATION_PERIOD_PARAM = "spark.cern.streaming.status.timeout";
+	
+	private transient final static Logger LOG = Logger.getLogger(Status.class.getName());
 
 	public static<K extends StatusKey, V, S extends StatusValue, R> StateDStream<K, V, S, R> map(
 			Class<K> keyClass,
@@ -80,7 +83,14 @@ public class Status {
 															.leftOuterJoin(filterOperations)
 															.filter(t -> t._2._2.isPresent())
 															.mapToPair(t -> new Tuple2<>(t._2._1, t._2._2.get()))
-															.foreachRDD(rdd -> rdd.foreachPartitionAsync(new ZookeeperStatusesOpertaionsF<K, V, S>(zooStatusesOpFProps)));
+															.foreachRDD(rdd -> {
+																long count = rdd.count();
+																
+																if(count > 0)
+																	LOG.info("Processing list operations (states " + count + ")");
+																
+																rdd.foreachPartitionAsync(new ZookeeperStatusesOpertaionsF<K, V, S>(zooStatusesOpFProps));
+															});
 		
 		return new StateDStream<>(statusStream);
 	}
