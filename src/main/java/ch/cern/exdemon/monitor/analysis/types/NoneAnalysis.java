e7c9f9286fc0c9a60485690b6d86879aea708b9a
dlanza <daniel.lanza@cern.ch>
2018-08-23 14:48:19 +0200

    NonoAnalysis: config(): warning if other params

--- src/main/java/ch/cern/exdemon/monitor/analysis/types/NoneAnalysis.java
+++ src/main/java/ch/cern/exdemon/monitor/analysis/types/NoneAnalysis.java
@@ -1,15 +1,22 @@
 package ch.cern.exdemon.monitor.analysis.types;
 
+import ch.cern.exdemon.components.ConfigurationResult;
 import ch.cern.exdemon.components.RegisterComponentType;
 import ch.cern.exdemon.metrics.Metric;
 import ch.cern.exdemon.monitor.analysis.Analysis;
 import ch.cern.exdemon.monitor.analysis.results.AnalysisResult;
 import ch.cern.exdemon.monitor.analysis.results.AnalysisResult.Status;
+import ch.cern.properties.Properties;
 
 @RegisterComponentType("none")
 public class NoneAnalysis extends Analysis {
 
 	private static final long serialVersionUID = 335288998662554717L;
+	
+	@Override
+	protected ConfigurationResult config(Properties properties) {
+	    return super.config(properties).merge(null, properties.warningsIfNotAllPropertiesUsed());
+	}
 
 	@Override
 	protected AnalysisResult process(Metric metric) {
