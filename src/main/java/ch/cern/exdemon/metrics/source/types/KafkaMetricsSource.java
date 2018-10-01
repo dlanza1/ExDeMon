be604880daa91801bd58512ce320320b15290172
dlanza <daniel.lanza@cern.ch>
2018-10-01 16:33:48 +0200

    KafkaMetricsSource: format

--- src/main/java/ch/cern/exdemon/metrics/source/types/KafkaMetricsSource.java
+++ src/main/java/ch/cern/exdemon/metrics/source/types/KafkaMetricsSource.java
@@ -28,51 +28,50 @@ import ch.cern.properties.Properties;
 public class KafkaMetricsSource extends MetricsSource {
 
     private static final long serialVersionUID = 4110858617715602562L;
-    
+
     private Map<String, Object> kafkaParams;
     private Set<String> kafkaTopics;
 
     @Override
     public ConfigurationResult config(Properties properties) {
-    	ConfigurationResult confResult = super.config(properties);
-    		
+        ConfigurationResult confResult = super.config(properties);
+
         kafkaParams = getKafkaConsumerParams(properties);
         kafkaTopics = new HashSet<String>(Arrays.asList(properties.getProperty("topics").split(",")));
-        
+
         return confResult.merge(null, properties.warningsIfNotAllPropertiesUsed());
     }
-    
+
     @Override
-	public JavaDStream<String> createJavaDStream(JavaStreamingContext ssc) {
-    		JavaInputDStream<ConsumerRecord<String, String>> inputStream = KafkaUtils.createDirectStream(
-                ssc,
+    public JavaDStream<String> createJavaDStream(JavaStreamingContext ssc) {
+        JavaInputDStream<ConsumerRecord<String, String>> inputStream = KafkaUtils.createDirectStream(ssc,
                 LocationStrategies.PreferConsistent(),
                 ConsumerStrategies.<String, String>Subscribe(kafkaTopics, kafkaParams));
-        
-        inputStream.foreachRDD(rdd -> {
-        			OffsetRange[] offsetRanges = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
 
-            		((CanCommitOffsets) inputStream.inputDStream()).commitAsync(offsetRanges);
-        		});
-        
+        inputStream.foreachRDD(rdd -> {
+            OffsetRange[] offsetRanges = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
+
+            ((CanCommitOffsets) inputStream.inputDStream()).commitAsync(offsetRanges);
+        });
+
         return inputStream.map(ConsumerRecord::value);
     }
 
     private Map<String, Object> getKafkaConsumerParams(Properties props) {
         Map<String, Object> kafkaParams = new HashMap<String, Object>();
-        
+
         kafkaParams.put("enable.auto.commit", Boolean.FALSE.toString());
         kafkaParams.put("key.deserializer", StringDeserializer.class);
         kafkaParams.put("value.deserializer", StringDeserializer.class);
-        
+
         Properties kafkaPropertiesFromConf = props.getSubset("consumer");
         for (Entry<Object, Object> kafkaPropertyFromConf : kafkaPropertiesFromConf.entrySet()) {
             String key = (String) kafkaPropertyFromConf.getKey();
             String value = (String) kafkaPropertyFromConf.getValue();
-            
+
             kafkaParams.put(key, value);
         }
-        
+
         return kafkaParams;
     }
 
